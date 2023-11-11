88afc567dc54f2a0ab3972af234fa84ec9aaa6bb
Jimmy Breen <jimmymbreen@gmail.com>
2023-11-11 16:56:57 -0330

    new files - download shell and HTML

---
+++ download.sh
@@ -0,0 +1,5 @@
+mkdir -p /cloud/project/workshop/data
+cd /cloud/project/workshop
+wget https://raw.githubusercontent.com/black-ochre-data-labs/2023-newfoundland-programming-workshops/main/population.csv -O data/population.csv
+wget https://raw.githubusercontent.com/black-ochre-data-labs/2023-newfoundland-programming-workshops/main/pelts.csv -O data/pelts.csv
+wget https://raw.githubusercontent.com/black-ochre-data-labs/2023-newfoundland-programming-workshops/main/coding.Rmd -O ./coding.Rmd
